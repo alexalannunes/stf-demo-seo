c0424f7e7236ca351572d75418545ed93c595289
Alex Alan Nunes <alexalannunesde_lima@apple.com>
2024-06-26 15:05:40 -0300

    google verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Head } from "next/document";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "STF - Supremo Tribunal Federal",
-  description: "O Supremo Tribunal Federal (STF) é a instância superior ou última instância do poder judiciário brasileiro; a que acumula tanto competências típicas de uma suprema corte, ou seja, um tribunal de última instância (popularmente conhecida como terceira instância), como as de um tribunal constitucional, ou seja, aquele que julga questões de constitucionalidade independentemente de litígios concretos.",
+  description:
+    "O Supremo Tribunal Federal (STF) é a instância superior ou última instância do poder judiciário brasileiro; a que acumula tanto competências típicas de uma suprema corte, ou seja, um tribunal de última instância (popularmente conhecida como terceira instância), como as de um tribunal constitucional, ou seja, aquele que julga questões de constitucionalidade independentemente de litígios concretos.",
 };
 
 export default function RootLayout({
@@ -16,6 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta
+        name="google-site-verification"
+        content="aEdmSv4Liad52ujAs70aLu_5IVpNmPWCcRTuAj34SjQ"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
